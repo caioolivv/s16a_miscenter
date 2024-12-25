b716ce8c45ef8943893d31b8f12d72c7ec6a96d6
Caio Lima de Oliveira <code.caio@limadeoliveira.me>
2024-12-25 19:53:21 -0300

    Update box side when matching catalogs

--- match_catalogs.py
+++ match_catalogs.py
@@ -1,9 +1,18 @@
 # # Setup libraries
 import os
+import sys
 import numpy as np
 from astropy.table import Table, join, vstack
 from tqdm import tqdm
+from scipy.optimize import fsolve
 
+from numcosmo_py import nc
+from numcosmo_py import ncm
+
+__name__ = "NcContext"
+
+ncm.cfg_init()
+ncm.cfg_set_log_handler(lambda msg: sys.stdout.write(msg) and sys.stdout.flush())
 
 # # Load the cluster and shear catalogs
 #
@@ -26,10 +35,51 @@ if not os.path.exists("clusters"):
 for cluster in tqdm(cluster_catalog):
     ra = cluster["ra"]
     dec = cluster["dec"]
+    z = cluster["z"]
+
+    cosmo = nc.HICosmoDEXcdm()
+
+    cosmo.params_set_default_ftype()
+    cosmo.omega_x2omega_k()
+
+    cosmo["H0"] = 69.7
+    cosmo["Omegab"] = 0.0464
+    cosmo["Omegac"] = 0.235
+    cosmo["w"] = -1.0
+    cosmo["Omegak"] = 0.00
+
+    prim = nc.HIPrimPowerLaw.new()
+    prim["ln10e10ASA"] = 3.02745
+    prim["n_SA"] = 0.9660
+
+    reion = nc.HIReionCamb.new()
+
+    cosmo.add_submodel(prim)
+    cosmo.add_submodel(reion)
+
+    dist = nc.Distance.new(6.0)
+    halo_position = nc.HaloPosition.new(dist)
+
+    halo_position.prepare(cosmo)
+
+    halo_position["ra"] = ra
+    halo_position["dec"] = dec
+    halo_position["z"] = z
+
+    half_box_side = fsolve(
+        lambda sep: halo_position.projected_radius_from_ra_dec(cosmo, ra, dec + sep)
+        - (
+            3.0
+            + halo_position.projected_radius_from_ra_dec(
+                cosmo, ra, dec + cluster["sep"]
+            )
+        ),
+        0.5,
+    )[0]
 
     cluster_shear_catalog = shear_catalog[
-        (np.abs(shear_catalog["ira"] - ra) < 0.2 / np.cos(np.radians(dec)))
-        & (np.abs(shear_catalog["idec"] - dec) < 0.2)
+        (np.abs(shear_catalog["ira"] - ra) < half_box_side / np.cos(np.radians(dec)))
+        & (np.abs(shear_catalog["idec"] - dec) < half_box_side)
     ]
 
     pz_catalogs = []
