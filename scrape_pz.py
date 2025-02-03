ce44c14c3837e951f31f57401578d95a9743ca50
Caio Lima de Oliveira <code.caio@limadeoliveira.me>
2025-02-03 15:15:09 -0300

    Update pz scraping script to scrape nnpz instead of frankenz

--- scrape_pz.py
+++ scrape_pz.py
@@ -5,23 +5,10 @@ from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-
-# # Setup options
-#
-# Adjust `photoz_method` to choose your preferred photo-z estimate. Also, be sure to add your
-# credentials to `auth` before running this notebook.
-
-# Choose your photo-z method
-PHOTO_Z_METHOD = "frankenz"
-# Insert your HSC user and password
+PHOTO_Z_METHOD = "nnpz"
 auth = HTTPBasicAuth("user", "pass")
-
 PZ_URL = "https://hsc-release.mtk.nao.ac.jp/archive/filetree/s16a-shape-catalog/"
 
-
-# # Downloading the data
-#
-# Now we create the `pz` directory and download the photo-z pdf bins definitions:
 pz_bins_request = requests.get(PZ_URL, auth=auth, timeout=10)
 pz_bins_soup = BeautifulSoup(pz_bins_request.text, "html.parser")
 
@@ -35,13 +22,10 @@ for file in pz_bins_soup.find_all("a"):
         )
 
         with open(f"pz/s16a_{PHOTO_Z_METHOD}_bins.fits", "wb") as f:
-            for chunk in pz_bins_definition_request.iter_content(chunk_size=1024):
+            for chunk in pz_bins_definition_request.iter_content(chunk_size=8192):
                 if chunk:
                     f.write(chunk)
 
-
-# Finally, we iterate through the HSC fields and download the tract data for the chosen photo-z
-# method. Each `fits` object is downloaded in its own field directory inside `pz`.
 pz_fields_request = requests.get(PZ_URL + "Sirius/", auth=auth, timeout=10)
 pz_fields_soup = BeautifulSoup(pz_fields_request.content, "html.parser")
 
