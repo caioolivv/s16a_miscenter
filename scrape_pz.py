46bf549e3d24c4b0ee3aad0fb5f4a0cd15356bd6
Caio Lima de Oliveira <code.caio@limadeoliveira.me>
2025-02-05 09:11:12 -0300

    Update scrape script to use aria2c and download mlz photoz

--- scrape_pz.py
+++ scrape_pz.py
@@ -1,11 +1,12 @@
 # # Setup libraries
 import os
+import subprocess
 import requests
 from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-PHOTO_Z_METHOD = "nnpz"
+PHOTO_Z_METHOD = "mlz"
 auth = HTTPBasicAuth("user", "pass")
 PZ_URL = "https://hsc-release.mtk.nao.ac.jp/archive/filetree/s16a-shape-catalog/"
 
@@ -21,38 +22,54 @@ for file in pz_bins_soup.find_all("a"):
             f"{PZ_URL}{file.get('href')}", stream=True, auth=auth, timeout=10
         )
 
-        with open(f"pz/s16a_{PHOTO_Z_METHOD}_bins.fits", "wb") as f:
-            for chunk in pz_bins_definition_request.iter_content(chunk_size=8192):
+        with open("pz/s16a_pz_pdf_bins.fits", "wb") as f:
+            for chunk in pz_bins_definition_request.iter_content(
+                chunk_size=10 * 1024 * 1024
+            ):
                 if chunk:
                     f.write(chunk)
 
 pz_fields_request = requests.get(PZ_URL + "Sirius/", auth=auth, timeout=10)
 pz_fields_soup = BeautifulSoup(pz_fields_request.content, "html.parser")
 
-for field in tqdm(pz_fields_soup.find_all("a")):
-    if field["href"] == "../":
-        continue
+URIS_FILE = "pz/uris.txt"
 
-    pz_tracts_request = requests.get(
-        PZ_URL + "Sirius/" + field["href"], auth=auth, timeout=10
-    )
-    pz_tracts_soup = BeautifulSoup(pz_tracts_request.content, "html.parser")
-
-    if not os.path.exists(f"pz/{field['href']}"):
-        os.makedirs(f"pz/{field['href']}")
-
-    for file in tqdm(pz_tracts_soup.find_all("a")):
-        if "frankenz" not in file["href"]:
+with open(URIS_FILE, "w", encoding="UTF8") as f:
+    for field in tqdm(pz_fields_soup.find_all("a")):
+        if field["href"] == "../":
             continue
 
-        pz_pdf_request = requests.get(
-            PZ_URL + "Sirius/" + field["href"] + file["href"],
-            stream=True,
-            auth=auth,
-            timeout=10,
+        pz_tracts_request = requests.get(
+            PZ_URL + "Sirius/" + field["href"], auth=auth, timeout=10
         )
+        pz_tracts_soup = BeautifulSoup(pz_tracts_request.content, "html.parser")
 
-        with open(f"pz/{field['href']}{file['href']}", "wb") as f:
-            for chunk in pz_pdf_request.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
+        if not os.path.exists(f"pz/{field['href']}"):
+            os.makedirs(f"pz/{field['href']}")
+
+        for file in tqdm(pz_tracts_soup.find_all("a")):
+            if PHOTO_Z_METHOD not in file["href"]:
+                continue
+
+            fits_url = PZ_URL + "Sirius/" + field["href"] + file["href"]
+
+            file_name = f"pz/{field['href']}{file['href'].split('_')[0]}_pz_pdf.fits"
+
+            f.write(fits_url + "\n")
+            f.write(f"  out={file_name}\n")
+
+subprocess.run(
+    [
+        "aria2c",
+        f"--http-user={auth.username}",
+        f"--http-passwd={auth.password}",
+        "--max-connection-per-server=16",
+        "--split=16",
+        "--max-tries=100",
+        "--retry-wait=10",
+        f"--input-file={URIS_FILE}",
+    ],
+    check=True,
+)
+
+subprocess.run(["rm", URIS_FILE], check=True)
